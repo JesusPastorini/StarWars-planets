9c44833fd1c404b40b88c231edd36853f5bb3144
JesusPastorini <juniorpastorini_@hotmail.com>
2023-06-02 06:10:48 -0300

    requisito 3

--- src/Hooks/FilterProvaider.js
+++ src/Hooks/FilterProvaider.js
@@ -5,6 +5,7 @@ import filterContext from '../context/createContext';
 function Provider({ children }) {
   const [planetsData, setPlanetsData] = useState([]);
   const [inputText, setInputText] = useState('');
+  const [numericFilter, setNumericFilter] = useState(null);
 
   const refresh = () => {
     fetch('https://swapi.dev/api/planets')
@@ -28,17 +29,46 @@ function Provider({ children }) {
   function handleChangeInputText(e) {
     setInputText(e);
   }
-
+  // Filtra por texto digitado
   function filterPlanets() {
     const filteredData = planetsData
       .filter((planet) => planet.name.toLowerCase().includes(inputText.toLowerCase()));
     return filteredData;
   }
 
+  // Aplica o filtro numérico
+  function applyNumericFilter(column, comparison, handleValue) {
+    const filteredData = planetsData.filter((planet) => {
+      // console.log(planet)
+      const planetValue = Number(planet[column]);
+      // console.log(planetValue)
+      if (comparison === 'maior que') {
+        return planetValue > handleValue;
+      } if (comparison === 'menor que') {
+        return planetValue < handleValue;
+      } if (comparison === 'igual a') {
+        return planetValue === handleValue;
+      }
+      return false;
+    });
+    console.log(filteredData);
+    return filteredData;
+  }
+  const filteredPlanets = numericFilter
+    ? applyNumericFilter(
+      numericFilter.column,
+      numericFilter.comparison,
+      numericFilter.handleValue,
+    )
+    : filterPlanets();
+
   const value = {
-    planetsData: filterPlanets(),
+    planetsData: filteredPlanets,
     inputText,
     handleChangeInputText,
+    applyNumericFilter: (column, comparison, handleValue) => {
+      setNumericFilter({ column, comparison, handleValue });
+    },
   };
 
   return (
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,22 +1,86 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import filterContext from '../context/createContext';
 
 export default function Filter() {
-  const { handleChangeInputText } = useContext(filterContext);
+  const { handleChangeInputText, applyNumericFilter } = useContext(filterContext);
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState('0');
+  const [filterResult, setFilterResult] = useState(null);
 
-  const handleFilterChange = ({ target }) => {
-    const { value } = target;
-    handleChangeInputText(value);
-    console.log(value);
+  const handleColumnChange = (event) => {
+    setColumn(event.target.value);
   };
 
+  const handleComparisonChange = (event) => {
+    setComparison(event.target.value);
+  };
+
+  const handleValueChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleFilterClick = () => {
+    applyNumericFilter(column, comparison, Number(value));
+    setFilterResult({ column, comparison, value });
+  };
+
+  const handleFilterChange = (event) => {
+    handleChangeInputText(event.target.value);
+  };
+
+  const optionsColumn = [
+    'population',
+    'orbital_period',
+    'diameter', 'rotation_period',
+    'surface_water'];
+  const optionsCompar = ['maior que', 'menor que', 'igual a'];
   return (
     <div>
-      <input
-        type="text"
-        onChange={ handleFilterChange }
-        data-testid="name-filter"
-      />
+      <div>
+        <input
+          type="text"
+          onChange={ handleFilterChange }
+          data-testid="name-filter"
+        />
+      </div>
+      <div>
+        <select onChange={ handleColumnChange } data-testid="column-filter">
+          {optionsColumn.map((option) => (
+            <option key={ option } value={ option }>
+              {option}
+            </option>
+          ))}
+        </select>
+
+        <select onChange={ handleComparisonChange } data-testid="comparison-filter">
+          {optionsCompar.map((compar) => (
+            <option key={ compar } value={ compar }>
+              {compar}
+            </option>
+          ))}
+        </select>
+
+        <input
+          type="number"
+          value={ value }
+          onChange={ handleValueChange }
+          data-testid="value-filter"
+        />
+
+        <button onClick={ handleFilterClick } data-testid="button-filter">
+          Filtrar
+        </button>
+      </div>
+      {filterResult && (
+        <span>
+          {filterResult.column}
+          {' '}
+          {filterResult.comparison}
+          {' '}
+          {filterResult.value}
+        </span>
+      )}
     </div>
   );
 }
